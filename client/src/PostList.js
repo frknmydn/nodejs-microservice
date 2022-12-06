439399a13dc63da4fd2e1da3f06b2c4c3e5b829a
furkan <furkan.meydan@hotmail.com>
2022-12-06 18:20:38 +0300

    postlar post.com'da gözüküyor. ama commentler gelmiyor. Path'te bir sıkıntı yok gibi gönderilen urllere bakmak lazım.

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,7 +8,7 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPost = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://posts.com/posts");
     
 
     setPosts(res.data);
--- posts/index.js
+++ posts/index.js
@@ -15,7 +15,7 @@ app.get('/posts',(req,res) =>{
 });
 
 
-app.post('/posts',async (req,res) =>{
+app.post('/posts/create',async (req,res) =>{
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
 
